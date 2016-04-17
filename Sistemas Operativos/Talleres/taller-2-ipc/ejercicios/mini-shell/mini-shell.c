fa2383c068edd2bbc8ebb5b6a427af6ec68052a9
laucha <laucha@laucha.(none)>
2016-04-17 13:12:37 -0300

    taller 2 avanzado

--- Sistemas Operativos/Talleres/taller-2-ipc/ejercicios/mini-shell/mini-shell.c
+++ Sistemas Operativos/Talleres/taller-2-ipc/ejercicios/mini-shell/mini-shell.c
@@ -7,13 +7,15 @@
 #include <unistd.h>     /* exit, fork */
 
 int run(char *program_name[], char **program_argv[], unsigned int count) {
-    pid_t child[count];
+    int child[count];
     int num_proc = 0;
     int soyhijo = 0;
     int pipes[count][2];
     char buf[1024];
     //char buf_sal[1024];
     char *newenviron[] = { NULL };
+    //int haydatos = 0;
+    //int status = 0;
     
     while (num_proc < count && soyhijo == 0) {
         //creo un nuevo pipe
@@ -30,27 +32,63 @@ int run(char *program_name[], char **program_argv[], unsigned int count) {
         }
     }
     if (soyhijo == 1) {
-        printf("Soy el hijo %d\n", num_proc);
-        if (num_proc > 0) {
-            dup2(0, pipes[num_proc-1][0]);
-            if (read(pipes[num_proc-1][0], buf, 1024) < 0) {
-                perror("leyendo datos");
+        //printf("Soy el hijo %d\n", num_proc);
+        for (int i = 0; i < num_proc; i++) {
+            close(pipes[i][1]);
+            if (i != num_proc-1) {
+                close(pipes[i][0]);
             }
         }
-        printf("Pude pasar. Soy el hijo %d\n", num_proc);
+        close(pipes[num_proc][0]);
+        close(0);
+        if (num_proc > 0) {
+            dup(pipes[num_proc-1][0]);
+            //if (read(pipes[num_proc-1][0], buf, 1024) < 0) {
+            //    perror("leyendo datos");
+            //}
+            //haydatos = 1;
+        }
+        //printf("Pude pasar. Soy el hijo %d\n", num_proc);
 
+        /*if (haydatos == 1) {
+            printf("Datos: %s\n", buf);
+            int noencontre = 1;
+            int j = 0;
+            while (noencontre) {
+                if (program_argv[num_proc][j] == NULL) {
+                    noencontre = 0;
+                    program_argv[num_proc][j] = buf;
+                    program_argv[num_proc][j+1] = NULL;
+                }
+                printf("%d: %s\n", j, program_argv[num_proc][j]);
+                j++;
+            }
+            printf("%d: %s\n", j, program_argv[num_proc][j]);
+        }*/
+        //printf("%s\n", program_argv[num_proc][0]);
+        //printf("%s\n", program_argv[num_proc][1]);
         dup2(pipes[num_proc][1], 1);
+        //perror("antes de ejecutar");
         execve(program_name[num_proc], program_argv[num_proc], newenviron);
+        //perror("despues de ejecutar");
         /*if (write(pipes[num_proc][1], buf_sal, sizeof(buf_sal)) < 0) {
             perror("escribiendo datos");
         }*/
     } else {
-        printf("Soy el padre\n");
-        dup2(1, pipes[count-1][0]);
+        for (int i = 0; i < count - 1; i++) {
+            close(pipes[i][1]);
+            close(pipes[i][0]);
+        }
+        close(pipes[count-1][1]);
+
+        //printf("Mi ultimo hijo: %d.\n", child[count-1]);
+        //dup2(pipes[count-1][0], 1);
+        //waitpid(child[count-1], &status, 1);
         if (read(pipes[count-1][0], buf, 1024) < 0) {
             perror("leyendo datos");
         }
-        printf("Llega: %s\n", buf);
+        printf("%s", buf);
+        //perror("Soy el padre saliendo");
     }
 
     return 0;
@@ -74,7 +112,7 @@ int main(int argc, char* argv[]) {
         awk_argv,
     };
 
-    unsigned int count = 1;
+    unsigned int count = 3;
 
     int status = run(program_name, program_argv, count);
 
